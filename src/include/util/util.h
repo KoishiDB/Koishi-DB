548b049c8682155976556a70478130686838cde7
SMAC-Zhang <zhanzhuqiong@163.com>
2022-10-12 09:30:27 +0800

    finish skiplist part
    Signed-off-by: SMAC-Zhang <zhanzhuqiong@163.com>

--- src/include/util/util.h
+++ src/include/util/util.h
@@ -13,18 +13,11 @@ namespace koishidb {
     // use Slice rather Slice& to make the metable_key untouched
     void ExtractInternalKey(Slice metable_key, Slice* internal_key);
 
-<<<<<<< HEAD
-    inline void ExtractUserKey(const Slice& internal_key, Slice* user_key, SequenceNumber* number) {
-        *user_key = Slice(internal_key.data(), internal_key.size() - 8); //
-        *number = *reinterpret_cast<const SequenceNumber*>(internal_key.data()) >> 8;
-    }
-=======
-    void ExtractUserKey(Slice& internal_key, Slice* user_key, SequenceNumber* number);
+    void ExtractUserKey(const Slice& internal_key, Slice* user_key, SequenceNumber* number);
 
     Slice CreateInternalKey(const Slice& user_key, SequenceNumber snapshot);
 
     Slice CreateMemtableKey(const Slice& user_key, SequenceNumber snapshot);
->>>>>>> 5a918a0c8282b38a195d2895dbded31c8081b132
 
     // init to make skiplist node height random
     inline void rng() {
